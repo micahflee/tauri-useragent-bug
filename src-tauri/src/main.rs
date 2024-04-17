74e06a86e17340bba1e7f2773da972c96abb0d08
Micah Lee <micah@micahflee.com>
2024-04-17 18:37:12 +0200

    reproduce bug

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,25 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::{LogicalPosition, LogicalSize, Manager, WebviewUrl};
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            let width = 800.;
+            let height = 600.;
+            let window = app.get_window("main").unwrap();
+            let _ = window.add_child(
+                tauri::webview::WebviewBuilder::new(
+                    "main3",
+                    WebviewUrl::External("https://www.whatsmyua.info/".parse().unwrap()),
+                )
+                .auto_resize(),
+                LogicalPosition::new(0., height / 2.),
+                LogicalSize::new(width, height / 2.),
+            )?;
+            Ok(())
+        })
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
